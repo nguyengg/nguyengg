671fcd7824c52bda93ff7eca6e5d5766760bc3de
Henry Nguyen <5065089+nguyengg@users.noreply.github.com>
2024-05-05 16:14:36 -0700

    tmux-rebase can support multiple search directories

--- scripts/tmux-rebase.py
+++ scripts/tmux-rebase.py
@@ -13,26 +13,27 @@ if __name__ == "__main__":
 
     parser.add_argument('-d', '--max-depth', default=2, type=int, metavar="depth",
                         help="The max depth to search for Git repos, default to 2")
-    parser.add_argument('dir', default=".", nargs='?',
-                        help="The root directory to start the search, default to current directory")
+    parser.add_argument('dirs', default=["."], nargs='+', metavar="dir",
+                        help="The root directories to start the search, default to current directory")
 
     svr = libtmux.Server()
 
     args = parser.parse_args()
     max_depth = args.max_depth - 1
-    for root, dirs, files in os.walk(args.dir):
-        # root=./my/repo
-        # n=my/repo
-        name = os.path.relpath(root, args.dir)
-        if name.count(os.path.sep) > max_depth:
-            del dirs[:]
-        if '.git' not in dirs:
-            continue
+    for dir in args.dirs:
+        for root, dirs, files in os.walk(dir):
+            # root=./my/repo
+            # name=my/repo
+            name = os.path.relpath(root, dir)
+            if name.count(os.path.sep) > max_depth:
+                del dirs[:]
+            if '.git' not in dirs:
+                continue
 
-        sess = svr.sessions.get(session_name=name, default=None)
-        if sess is None:
-            sess = svr.new_session(session_name=name, start_directory=root, window_name=name)
-            win = sess.windows.get(window_name=name)
-        else:
-            win = sess.new_window(window_name=name, start_directory=root)
-        win.active_pane.send_keys('git pull --rebase')
+            sess = svr.sessions.get(session_name=name, default=None)
+            if sess is None:
+                sess = svr.new_session(session_name=name, start_directory=root, window_name=name)
+                win = sess.windows.get(window_name=name)
+            else:
+                win = sess.new_window(window_name=name, start_directory=root)
+            win.active_pane.send_keys('git pull --rebase')
