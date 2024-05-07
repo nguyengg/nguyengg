13ed50b7d8f38f597be41091a97e5b5eeca529ab
Henry <5065089+nguyengg@users.noreply.github.com>
2024-05-07 01:11:54 -0700

    Use asyncio to display results of git rebase

--- scripts/tmux-rebase.py
+++ scripts/tmux-rebase.py
@@ -1,10 +1,39 @@
 #!/usr/bin/env python3
 
 import argparse
-import libtmux
+import asyncio
 import os
+import sys
 
-if __name__ == "__main__":
+import libtmux
+
+svr = libtmux.Server()
+
+
+async def rebase(root, name):
+    sess = svr.sessions.get(session_name=name, default=None)
+    if sess is None:
+        pane = svr.new_session(session_name=name, start_directory=root).active_pane
+    else:
+        pane = sess.new_window(start_directory=root).active_pane
+
+    proc = await asyncio.create_subprocess_shell(
+        f'git -C {root} pull --rebase',
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+    stdout, stderr = await proc.communicate()
+
+    if proc.returncode == 0:
+        txt = stdout.decode().splitlines()[-1] if stdout else 'done with no output'
+        print(f'{name}: {txt}')
+        pane.send_keys('git pull --rebase')
+    else:
+        txt = stderr.decode().splitlines()[-1] if stderr else f'exits with code {proc.returncode}'
+        print(f'{name}: {txt}', file=sys.stderr)
+        pane.send_keys('git pull --rebase')
+
+
+async def main():
     parser = argparse.ArgumentParser(
         prog="tmux-rebase.py",
         description="Find all Git repos, create a new tmux session (or window if a session with same name already "
@@ -16,12 +45,11 @@ if __name__ == "__main__":
     parser.add_argument('dirs', default=["."], nargs='+', metavar="dir",
                         help="The root directories to start the search, default to current directory")
 
-    svr = libtmux.Server()
-
     args = parser.parse_args()
     max_depth = args.max_depth - 1
+
+    tasks = list()
     for top in args.dirs:
-        print(top)
         for root, dirs, files in os.walk(top):
             # root=./my/repo
             # name=my/repo
@@ -31,11 +59,10 @@ if __name__ == "__main__":
             if '.git' not in dirs:
                 continue
 
-            sess = svr.sessions.get(session_name=name, default=None)
-            if sess is None:
-                print(f"\t{name}: new session")
-                pane = svr.new_session(session_name=name, start_directory=root).active_pane
-            else:
-                print(f"\t{name}: new window")
-                pane = sess.new_window(start_directory=root).active_pane
-            pane.send_keys('git pull --rebase')
+            tasks.append(asyncio.create_task(rebase(root, name)))
+
+    await asyncio.gather(*tasks)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
